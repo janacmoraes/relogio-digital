f59251e15c53095f34409ccfcdb277e791fdfba2
janacmoraes <janacsmoraes@gmail.com>
2023-05-17 11:23:08 -0300

    correção das variáveis

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,16 @@ const segundos = document.getElementById('segundos');
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hora = dateToday.getHours();
-    let minutos = dateToday.getMinutes();
-    let segundos = dateToday.getSeconds();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
 
     if(hora < 10) hora = '0' + hora;
 
-    if(minutos < 10) minutos = '0' + minutos;
+    if(min < 10) minutos = '0' + min;
 
-    if(segundos < 10) segundos = '0' + segundos;
+    if(seg < 10) segundos = '0' + seg;
 
     horas.textContent = hora;
-    minutos.textContent = minutos;
-    segundos.textContent = segundos;
+    minutos.textContent = min;
+    segundos.textContent = seg;
 })
